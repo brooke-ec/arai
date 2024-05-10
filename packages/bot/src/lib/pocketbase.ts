fc8fb7980d5eea9e9dca172d6bae22b91ce46bc0
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-05-10 14:33:08 +0100

    feat: sync member function

--- packages/bot/src/lib/pocketbase.ts
+++ packages/bot/src/lib/pocketbase.ts
@@ -1,5 +1,5 @@
 import type { TypedPocketBase } from "./pocketbase.d";
 import PocketBase from "pocketbase";
 
-export const pocketbase: TypedPocketBase = new PocketBase(process.env.PB_TYPEGEN_URL);
-pocketbase.admins.authWithPassword(process.env.PB_TYPEGEN_EMAIL!, process.env.PB_TYPEGEN_PASSWORD!);
+export const pb: TypedPocketBase = new PocketBase(process.env.PB_TYPEGEN_URL);
+pb.admins.authWithPassword(process.env.PB_TYPEGEN_EMAIL!, process.env.PB_TYPEGEN_PASSWORD!);
---
+++ packages/bot/src/lib/utils.ts
@@ -0,0 +1,22 @@
+import { MemberRecord } from "./pocketbase.d";
+import { pb } from "./pocketbase";
+import { User } from "discord.js";
+import { ofetch } from "ofetch";
+
+export async function syncMember(user: User): Promise<MemberRecord> {
+	const old = (await pb.collection("member").getList(1, 1, { filter: `snowflake="${user.id}"` })).items[0];
+
+	const payload = new FormData();
+	payload.append("snowflake", user.id);
+	payload.append("name", user.displayName);
+
+	// Only synchronise avatar if it has changed
+	if (!old?.avatar.startsWith(`${user.avatar}`)) {
+		const avatarBlob = await ofetch(user.avatarURL({ extension: "webp" }));
+		const avatarFile = new File([avatarBlob], `${user.avatar}.webp`);
+		payload.append("avatar", avatarFile);
+	}
+
+	if (old) return await pb.collection("member").update(old.id, payload);
+	return await pb.collection("member").create(payload);
+}
